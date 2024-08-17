27ca0eac412adf7bddbbfe6749a853c6848c6ff7
BirajakBiswas <115551368+BirajakBiswas@users.noreply.github.com>
2024-08-17 08:39:24 +0000

    ecom website

--- less/less/urls.py
+++ less/less/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+# less/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls')),
+    path('', lambda request: redirect('shop/')),  # This redirects / to /shop/
+    path('blog/', include('blog.urls')),
+
 ]
+
