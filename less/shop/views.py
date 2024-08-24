ae8dee3c8d8d5c60997a7230ecf91107da1a9aac
BirajakBiswas <115551368+BirajakBiswas@users.noreply.github.com>
2024-08-24 14:02:20 +0000

    category

--- less/shop/views.py
+++ less/shop/views.py
@@ -6,12 +6,19 @@ from  math import ceil
 
 def index(request):
     products=Product.objects.all()
-    print(products)
-    n=len(products)
-    nSlides=n//4+ ceil((n/4)-(n//4))
+    allProds=[]#basically now we are trying to sort out items in our website based on category
+    catprods=Product.objects.values('category','id')#basically sorting depends on factors category and id
+    cats={item["category"]for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+       
+    
     #params ={'no_of_slides':nSlides,'range':range(1,nSlides),'product':products}
-    allProds=[[products,range(1,nSlides),nSlides],
-             [products,range(1,nSlides),nSlides]]
+    # #allProds=[[products,range(1,nSlides),nSlides],
+    #          [products,range(1,nSlides),nSlides]]
     params={'allProds':allProds}
               
     return render(request,'shop/index.html',params)
